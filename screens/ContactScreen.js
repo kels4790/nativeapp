a0d428486808a8da8f9e2be1f583a653095a8f37
User <user@MacBook-Pro.local>
2022-12-12 15:18:32 -0500

    workshopt2

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,8 +1,21 @@
 import { ScrollView } from "react-native-gesture-handler";
+import { Card } from 'react-native-elements';
+import { Text } from "react-native";
 
 const ContactScreen = () => {
     return (
-        <ScrollView></ScrollView>
+        <ScrollView>
+            <Card.Title style={{paddingTop: 15}}>Contact Information</Card.Title>
+            <Card.Divider />
+            <Card wrapperStyle={{ margin: 10 }}>
+                <Text>1 Nucamp Way</Text>
+                <Text>Seattle, WA 98001</Text>
+                <Text style={{marginBottom: 10}}>U.S.A</Text>
+
+                <Text>Phone: 1-206-555-1234</Text>
+                <Text>Email: campsites@nucamp.co</Text>
+            </Card>
+        </ScrollView>
     )
 }
 
