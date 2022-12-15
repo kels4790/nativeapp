d99836ad305449b211f9d36b7acabee89341f3ef
User <user@MacBook-Pro.local>
2022-12-14 22:41:25 -0500

    drawer

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -1,13 +1,14 @@
-import { Platform, StyleSheet, View } from 'react-native';
+import { Image, Text, Platform, StyleSheet, View } from 'react-native';
 import Constants from 'expo-constants';
 import CampsiteInfoScreen from "./CampsiteInfoScreen";
 import DirectoryScreen from "./DirectoryScreen";
 import { createStackNavigator } from '@react-navigation/stack';
-import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createDrawerNavigator, DrawerContentScrollView, DrawerItemList} from '@react-navigation/drawer';
 import HomeScreen from './HomeScreen';
 import AboutScreen from './AboutScreen';
 import ContactScreen from './ContactScreen';
 import { Icon } from 'react-native-elements'
+import logo from '../assets/images/logo.png';
 
 
 const Drawer = createDrawerNavigator();
@@ -127,11 +128,26 @@ const ContactNavigator = () => {
     )
 }
 
+const CustomDrawerContent = (props) => (
+    <DrawerContentScrollView {...props}>
+        <View style={styles.drawerHeader}>
+            <View style={{ flex: 1 }}>
+                <Image source={logo} style={styles.drawerImage} />
+            </View>
+            <View style={{ flex: 2}}>
+                <Text style={styles.drawerHeaderText}>NuCamp</Text>
+            </View>
+        </View>
+        <DrawerItemList {...props} labelStyle={{ fontWeight: 'bold'}} />
+    </DrawerContentScrollView>
+)
+
 const Main = () => {
     return (
         <View style={{flex: 1, paddingtop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight}}>
            <Drawer.Navigator
                 initialRouteName='Home'
+                drawerContent={CustomDrawerContent}
                 drawerStyle={{backgroundColor: '#CEC8FF'}}
            >
                 <Drawer.Screen 
@@ -201,6 +217,19 @@ const Main = () => {
 }
 
 const styles = StyleSheet.create({
+    drawerHeader: {
+        backgroundColor: '#5637DD',
+        height: 140, 
+        alignItems: 'center', 
+        justifyContent: 'center',
+        flex: 1, 
+        flexDirection: 'row'
+    },
+    drawerHeaderText: {
+        color: '#fff',
+        fontSize: 24, 
+        fontWeight: 'bold'
+    },
     stackIcon: {
         marginLeft: 10, 
         color: '#fff', 
