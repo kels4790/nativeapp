806d846b2f2e269d7f3b8ca66985c3757660c89a
User <user@MacBook-Pro.local>
2022-12-18 13:15:56 -0500

    swipe

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -16,6 +16,7 @@ import { fetchCampsites } from '../features/campsites/campsitesSlice';
 import { fetchPromotions } from '../features/promotions/promotionsSlice';
 import { fetchComments } from '../features/comments/commentsSlice';
 import ReservationScreen from './ReservationScreen';
+import FavoritesScreen from './FavoritesScreen';
 
 const Drawer = createDrawerNavigator();
 
@@ -160,6 +161,34 @@ const ReservationNavigator = () => {
     )
 }
 
+
+const FavoritesNavigator = () => {
+    const Stack = createStackNavigator();
+
+    return(
+        <Stack.Navigator
+            screenOptions={screenOptions}
+        >
+            <Stack.Screen 
+                name='Favorites'
+                component={FavoritesScreen}
+                options={({ navigation }) => ({
+                    title: 'Favorite Campsites',
+                    headerLeft: ()  => (
+                        <Icon 
+                            name='heart'
+                            type='font-awesome'
+                            iconStyle={styles.stackIcon}
+                            onPress={() => navigation.toggleDrawer()}
+                        />
+                    )
+                })}
+            />
+        </Stack.Navigator>
+    )
+}
+
+
 const CustomDrawerContent = (props) => (
     <DrawerContentScrollView {...props}>
         <View style={styles.drawerHeader}>
@@ -230,6 +259,22 @@ const Main = () => {
                         drawerIcon:({color}) => (
                             <Icon 
                                 name='tree'
+                                type='font-awesome'
+                                iconStyle={{width: 24}}
+                                color={color}
+                            />
+                        ) 
+                    }}
+                />
+
+                <Drawer.Screen 
+                    name='Favorites'
+                    component={FavoritesNavigator}
+                    options={{
+                        title: 'My Favorites',
+                        drawerIcon:({color}) => (
+                            <Icon 
+                                name='heart'
                                 type='font-awesome'
                                 iconStyle={{width: 24}}
                                 color={color}
