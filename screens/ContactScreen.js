b4feeaf58bb19980773b6492d6078046a110f61c
User <user@MacBook-Pro.local>
2022-12-29 20:03:08 -0500

    email

--- screens/ContactScreen.js
+++ screens/ContactScreen.js
@@ -1,10 +1,20 @@
 import { ScrollView } from "react-native-gesture-handler";
-import { Card } from 'react-native-elements';
+import { Card, Button, Icon } from 'react-native-elements';
 import { Text } from "react-native";
 import * as Animatable from 'react-native-animatable';
-
+import * as MailComposer from 'expo-mail-composer';
 
 const ContactScreen = () => {
+
+    const sendMail = () => {
+        MailComposer.composeAsync({
+            recipients: ['campsites@nucamp.com'],
+            subject: 'Inquiry',
+            body: 'To Whom it May Conceern: '
+        });
+    }
+
+
     return (
         <ScrollView>
                <Animatable.View
@@ -21,6 +31,19 @@ const ContactScreen = () => {
 
                 <Text>Phone: 1-206-555-1234</Text>
                 <Text>Email: campsites@nucamp.co</Text>
+                <Button 
+                    title='Send Email'
+                    buttonStyle={{backgroundColor: '#5637DD', margin: 40}}
+                    icon={
+                        <Icon 
+                            name="envelope-o"
+                            type="font-awesome"
+                            color='#fff'
+                            iconStyle={{marginRight: 10}}
+                        />
+                    }
+                    onPress={()=> sendMail()}
+                />
             </Card>
             </Animatable.View>
         </ScrollView>
