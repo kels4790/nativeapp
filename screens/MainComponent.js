6c0af69235c9c4db1a6e786cb948400f7d496183
User <user@MacBook-Pro.local>
2022-12-31 08:42:44 -0500

    workshopweek4

--- screens/MainComponent.js
+++ screens/MainComponent.js
@@ -245,8 +245,8 @@ const Main = () => {
         dispatch(fetchComments());
      });
 
-     useEffect(() => {
-        NetInfo.fetch().then((connectionInfo) =>{
+     const showNetInfo = async () => {
+        await NetInfo.fetch().then((connectionInfo) =>{
             Platform.OS === 'ios' ? Alert.alert('Initial Network Connectivity Type', connectionInfo.type)
             : ToastAndroid.show(
                 'Initial Network Connectivity Type: ' + 
@@ -255,6 +255,10 @@ const Main = () => {
             )
         });
 
+     }
+
+     useEffect(() => {
+        showNetInfo();
         const unsubscribeNetInfo = NetInfo.addEventListener(
             (connectionInfo) => {
                 handleConnectivityChange(connectionInfo);
